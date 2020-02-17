75d3f6e44fecb524a2eb8e798b7d4d8328f8362f
EC2 Default User <ec2-user@ip-172-31-14-47.us-east-2.compute.internal>
2020-02-17 10:47:46 +0000

    part 3 completed

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,25 +12,61 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings=Movie.all_ratings
-    if params[:order]=="title"
-      @movies=Movie.all.order(params[:order])
-      @highlight_title = "hilite"
-    elsif params[:order]=="release_date"
-      @movies=Movie.all.order(params[:order])
-      @highlight_date = "hilite"
+    
+    #redirect flag is used to indicate if a url should be redirected according to session values
+    redirectFlag=0
+    
+    if params[:order]
+      @orderList=params[:order]
     else
-      @movies = Movie.all
+      @orderList=session[:order]
     end
     
+    #setting redirect flag if param value is not present
+    if (params[:order]==nil && session[:order]!=nil)
+      redirectFlag=1
+    end
+    
+    #updating session
+    if params[:order]!= session[:order]
+      session[:order]=@orderList
+    end
+    if @orderList=="title"
+      @movies=Movie.all.order(@orderList)
+      @highlight_title = "hilite"
+    elsif @orderList=="release_date"
+      @movies=Movie.all.order(@orderList)
+      @highlight_date = "hilite"
+    else
+    @movies = Movie.all
+    end
+    
+    #filtering the movies
     if params[:ratings]
       @ratings=params[:ratings]
       @movies=@movies.where(rating: @ratings.keys)
     else
-      @ratings=Hash[@all_ratings.collect {|rating| [rating, rating]}]
-      @movies=@movies
+      if session[:ratings]
+        @ratings=session[:ratings]
+        @movies=@movies.where(rating: @ratings.keys)
+        redirectFlag=1 #setting redirect flag if param value is not present
+      else
+        @ratings=Hash[@all_ratings.collect {|rating| [rating, rating]}] #setting rating to all ratings as initially all boxes should be checked
+        @movies=@movies
+      end
     end
-    
+    #updating session 
+    if @ratings != session[:ratings]
+      session[:ratings]=@ratings
+    end
+    #redirecting the url according to the values in the session variable if there is no parameter values
+    if redirectFlag==1
+      flash.keep
+      redirect_to movies_path(order: session[:order],ratings: session[:ratings])
+    end
   end
+  
+  
 
   def new
     # default: render 'new' template
